53b2208861d99068b23206050423c133ea9cf404
Darkpayne <joshuaclifford4336@yahoo.com>
2025-05-02 10:19:49 +0100

    update endpoints

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,8 @@
-export const STUDENT_API_URL = "http://localhost:3000/api/students";
-export const AUTH_API_URL = "http://localhost:3000/api/auth";
+export const STUDENT_API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://student-information-management-system-sigma.vercel.app/api/students"
+    : "http://localhost:3000/api/students";
+export const AUTH_API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://student-information-management-system-sigma.vercel.app/api/auth"
+    : "http://localhost:3000/api/auth";
